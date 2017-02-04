02977bb67f254df78d1dc068e07a827e1d031982
efyx <n.trani@weelya.com>
2017-02-04 12:11:59 +0100

    Fix some build issues

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ clang++ VanillaJS/main.cpp -o VanillaRobot \
     -std=c++11 \
     -ljs_static -lmozglue -lnspr4 -lz 
 
-clang++ IDL/main.cpp IDL/JSRobot.cpp IDL/JSRobotBase.cpp -O IDLRobot \
+clang++ -v IDL/main.cpp IDL/JSRobot.cpp -o IDLRobot \
     -I$NIDIUM_PATH/src/ \
     -I$NIDIUM_PATH/src/libapenetwork/src/ \
     -I$NIDIUM_PATH/third-party/mozilla-central/js/src/obj/dist/include/ \
@@ -30,4 +30,4 @@ clang++ IDL/main.cpp IDL/JSRobot.cpp IDL/JSRobotBase.cpp -O IDLRobot \
     -L$NIDIUM_PATH/src/libapenetwork/gyp/build/Release/ \
     -include $NIDIUM_PATH/build/third-party/js-config.h \
     -std=c++11 \
-    -lnidiumcore -lnetwork -ljs_static -lmozglue -lnspr4 -lcares -lhttp_parser -lssl -lcrypto -lzip -lm -lpthread -lleveldb -lz
+    -lnidiumcore -ljs_static -lmozglue -lnspr4 -lcares -lhttp_parser -lssl -lcrypto -lm -lpthread -lleveldb -lz -lnetwork 
